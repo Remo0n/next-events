87c3b4a5c1579265b96a83d60800ec1ae3a4cea3
Remon <remoon.peter@gmail.com>
2023-02-02 23:02:57 +0200

    edit the component structure

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import classes from "./event-item.module.css";
+
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const readableDate = new Date(date).toLocaleDateString("en-US", {
@@ -7,22 +9,22 @@ const EventItem = (props) => {
     month: "long",
     year: "numeric",
   });
-  const formattedAddress = location.replace(", ", "/n");
+  const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
   return (
-    <li key={id}>
+    <li key={id} className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{readableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
