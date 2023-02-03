85eb1112d45d8d38615b41a26a23a5b0834ed48b
Remon <remoon.peter@gmail.com>
2023-02-03 22:05:54 +0200

    adding single event component and styling

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,33 @@
+import { useRouter } from "next/router";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data";
+
 const EventDetailPage = () => {
-  return <div>EventDetailPage</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found!!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailPage;
